6e689bddd61446e92111da0725a67814a7f574d5
user <user@DESKTOP-L6MO5E4>
2023-08-14 07:34:41 +0900

    수정중

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ public class Main extends HttpServlet{
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.getRequestDispatcher("/WEB-INF/main.jsp").forward(req, resp);
+		req.getRequestDispatcher("./WEB-INF/main.jsp").forward(req, resp);
 	}
 
 	@Override
